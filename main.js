f14c52f4811af3bed359efc6ff1b8c7bc0cbb4bb
forsythe <fjorsyth@gmail.com>
2016-10-01 14:25:15 +0800

    added test functions

--- main.js
+++ main.js
@@ -1,33 +1,183 @@
-Listings = new Mongo.Collection('listings');
-//loops
+Listings1 = new Mongo.Collection('listings1');
+Listings2 = new Mongo.Collection('listings2');
+Listings3 = new Mongo.Collection('listings3');
+
+BuySell1 = new Mongo.Collection('buySell1');
+BuySell2 = new Mongo.Collection('buySell2');
+BuySell3 = new Mongo.Collection('buySell3');
+
+var team_uid = 'DXSloWjyT-EKW7NnGPDr2Q';
+
 if (Meteor.isServer) {
+    Listings1.remove({});
+    Listings2.remove({});
+    Listings3.remove({});
 
-}
-var myInt;
-Meteor.methods({
-    'start': function () {
-        myInt = Meteor.setInterval(function () {
-            Listings.remove({});
-            var response = HTTP.get('http://cis2016-exchange1.herokuapp.com/api/market_data/');
+    //Session data (essentially global variables) call the function first to update them
 
-            _.each(response.data, function (item) {
-                //console.log(item);
-                Listings.insert(item);
+    var myInt;
+    Meteor.methods({
+        'start': function () {
+            console.log("START FUNCTION CALLED");
+            myInt = Meteor.setInterval(function () {
+                for (var k = 1; k <= 3; k++) {
+                    var response = HTTP.get('http://cis2016-exchange' + k + '.herokuapp.com/api/market_data/');
+                    _.each(response.data, function (item) {
+                        //console.log(item);
+                        switch (k) {
+                            case 1:
+                                Listings1.insert(item);
+                                break;
+                            case 2:
+                                Listings2.insert(item);
+                                break;
+                            case 3:
+                                Listings3.insert(item);
+                                break;
+                        }
+                    });
+                    //console.log(Listings.find().fetch());
+                    console.log("Updated market for exchange: " + k);
+                }
+            }, 1000);
+        },
+        'stop': function () {
+            console.log("STOP FUNCTION CALLED");
+            Meteor.clearInterval(myInt);
+            for (var k = 0; k < 99999; k++) {
+                Meteor.clearInterval(k);
+            }
+        },
+        'updateBuySellData': function (exchangeNum, symbol) {
+            console.log("Updating buy/sell data for symbol: " + symbol + " on exchange: " + exchangeNum);
+            var response = HTTP.get('http://cis2016-exchange' + exchangeNum + '.herokuapp.com/api/market_data/' + symbol);
+            console.log(response.data);
+            switch (exchangeNum) {
+                case 1:
+                    BuySell1.remove();
+                    BuySell1.insert(item);
+                    break;
+                case 2:
+                    BuySell2.remove();
+                    BuySell2.insert(item);
+                    break;
+                case 3:
+                    BuySell3.remove();
+                    BuySell3.insert(item);
+                    break;
+            }
+        },
+        'updateExchangeData': function (exchangeNum) {
+            console.log("Getting data from exchange: " + exchangeNum);
+            var response = HTTP.get('http://cis2016-exchange' + exchangeNum + '.herokuapp.com/api/market_data');
+            console.log(response.data);
+            switch (exchangeNum) {
+                case 1:
+                    BuySell1.remove();
+                    BuySell1.insert(item);
+                    break;
+                case 2:
+                    BuySell2.remove();
+                    BuySell2.insert(item);
+                    break;
+                case 3:
+                    BuySell3.remove();
+                    BuySell3.insert(item);
+                    break;
+            }
+        },
+        'marketBuy': function (exchange, symbol, qty) {
+            var postData = {
+                data: {
+                    team_uid: team_uid,
+                    symbol: symbol,
+                    qty: qty,
+                    order_type: 'market',
+                    side: 'buy'
+                }
+            };
+            var response = HTTP.post('http://cis2016-exchange' + exchange + '.herokuapp.com/api/orders', postData, function (error, response) {
+                if (error) {
+                    console.log(error);
+                } else {
+                    console.log(response);
+                }
             });
-            //console.log(Listings.find().fetch());
-            //console.log("********");
-        }, 1000);
-    },
-    'stop': function () {
-        Meteor.clearInterval(myInt);
-    },
-    'getBuySellData': function (exchangeNum, symbol) {
-        var response = HTTP.get('http://cis2016-exchange' + exchangeNum + '.herokuapp.com/api/market_data/' + symbol);
-        console.log(response);
-        Session.set("buySellData", response);
-    }
-});
+        },
+        'marketSell': function (exchange, symbol, qty) {
+            var postData = {
+                data: {
+                    team_uid: team_uid,
+                    symbol: symbol,
+                    qty: qty,
+                    order_type: 'market',
+                    side: 'sell'
+                }
+            };
 
+            var response = HTTP.post('http://cis2016-exchange' + exchange + '.herokuapp.com/api/orders', postData, function (error, response) {
+
+                if (error) {
+                    console.log(error);
+                } else {
+                    console.log(response);
+                }
+            });
+        },
+        'limitBuy': function (exchange, symbol, qty, price) {
+            var postData = {
+                data: {
+                    team_uid: team_uid,
+                    symbol: symbol,
+                    qty: qty,
+                    order_type: 'limit',
+                    side: 'buy',
+                    price: price
+                }
+            };
+
+            var response = HTTP.post('http://cis2016-exchange' + exchange + '.herokuapp.com/api/orders', postData, function (error, response) {
+
+                if (error) {
+                    console.log(error);
+                } else {
+                    console.log(response);
+                }
+            });
+        },
+        'limitSell': function (exchange, symbol, qty, price) {
+            var postData = {
+                data: {
+                    team_uid: team_uid,
+                    symbol: symbol,
+                    qty: qty,
+                    order_type: 'limit',
+                    side: 'sell',
+                    price: price
+                }
+            };
+
+            var response = HTTP.post('http://cis2016-exchange' + exchange + '.herokuapp.com/api/orders', postData, function (error, response) {
+
+                if (error) {
+                    console.log(error);
+                } else {
+                    console.log(response);
+                }
+            });
+        },
+        'deleteLimit': function (order_id) {
+            var response = HTTP.call('DELETE', 'http://cis2016-exchange1.herokuapp.com/api/orders/' + order_id, {}, function (error, response) {
+
+                if (error) {
+                    console.log(error);
+                } else {
+                    console.log(response);
+                }
+            });
+        }
+    });
+}
 if (Meteor.isClient) {
     Template.buttons.events({
         'click .start': function () {
@@ -35,7 +185,16 @@ if (Meteor.isClient) {
         },
         'click .stop': function () {
             Meteor.call('stop');
-            Meteor.call('getBuySellData', 1, 0001);
+        },
+        'click .test': function () {
+            Meteor.call('updateBuySellData', "1", "0001");
+        },
+        'click .testAkhan': function () {
+            Meteor.call('marketBuy', 1, '0388', 10);
+            // Meteor.call('marketSell', 1, '0388', 10);
+            // Meteor.call('limitBuy', 1, '0388', 10, 0);
+            // Meteor.call('limitSell', 1, '0388', 10, 100000);
+            // Meteor.call('deleteLimit', '596c80a2-3217-4845-8fc2-f4b868555115');
         }
     });
 
