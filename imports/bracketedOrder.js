551f119bf1aadc71f661a11ffa64f0294f30c06c
Gian <gianmiguel98@yahoo.com>
2016-10-01 14:34:52 +0800

    Updated bracketedOrder algorithm

--- imports/bracketedOrder.js
+++ imports/bracketedOrder.js
@@ -6,17 +6,18 @@ var min, max;
 var exchange = 1;
 var orderUrl = 'http://cis2016-exchange' + exchange + '.herokuapp.com/api/orders';
 var symbols = ['0001', '0005', '0386', '0388', '3988'];
-var orderType;
-var qty;
-var price;
+var sideType = ['buy', 'sell'];
+var qty = 100;
+var orderType = ['market', 'limit'];
+var price = 0;
 
 var setMin = function (i) {
     min = i;
-}
+};
 
 var setMax = function(i) {
     max = i;
-}
+};
 
 var setOrder = function() {
     for(var i = 1; i <=3; i++)
@@ -24,20 +25,25 @@ var setOrder = function() {
         exchange = i;
 
         for(var j = 0; j < 5; j++) {
+            //TODO: IF (buy and price <= min) or (sell and price >= max)
+            //TODO:
             var postData = {
                 data: {
                     team_uid: 'kKcYIo6QignhVwziE889Mg',
                     symbol: symbols[j],
+                    side: sideType,
+                    qty: 100,
+                    order_type: orderType[0];
+        }
+
+            var response = HTTP.post('http://cis2016-exchange'+exchange+'.herokuapp.com/api/orders', postData, function( error, response ) {
+                if (error) {
+                    console.log(error);
+                } else {
+                    console.log(response);
                 }
-            };
+            });
+        }
         }
     }
-
-    var response = HTTP.post('http://cis2016-teamtracker.herokuapp.com/api/teams', postData, function( error, response ) {
-        if (error) {
-            console.log(error);
-        } else {
-            console.log(response);
-        }
-    });
 };
